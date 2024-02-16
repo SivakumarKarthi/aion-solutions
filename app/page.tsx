0fba0ed431727e618f02226bd0c1d44e3c20e565
Archie Mourad <mouradarchie@gmail.com>
2024-02-16 12:47:09 +1000

    modifications to 'navbar' and added slogan

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,15 @@
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <div className="flex items-center h-dvh">
+        <div className="mx-16">
+          <h1 className="font-bold text-5xl text-rose-100">
+            Composting today,
+            <br />
+            for tomorrow.
+          </h1>
+        </div>
+      </div>
+    </main>
+  );
 }
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,8 +4,12 @@ import Logo from "@/public/logo.svg";
 
 export default function Navbar() {
   return (
-    <div className="border-b border-rose-100 p-4">
-      <Image src={Logo} alt="Aion Solutions" className="scale-50" />
+    <div className="fixed w-full">
+      <div className="border-b border-rose-100">
+        <div className="p-4">
+          <Image src={Logo} alt="Aion Solutions" className="scale-50" />
+        </div>
+      </div>
     </div>
   );
 }
