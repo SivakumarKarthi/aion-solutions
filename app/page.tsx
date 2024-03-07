31d6762f8b1e9849beee5d629c982e40016ebe64
Archie Mourad <mouradarchie@gmail.com>
2024-03-07 15:49:26 +1000

    'Map' unhidden with title

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,10 @@ import { Map } from "@/components";
 
 export default function Home() {
   return (
-    <main>
+    <main className="text-rose-100">
       <div className="flex items-center h-dvh bg-field bg-bottom">
         <div className="mx-16">
-          <h1 className="font-bold md:text-5xl text-4xl text-rose-100">
+          <h1 className="font-bold md:text-5xl text-4xl">
             Composting today,
             <br />
             for tomorrow.
@@ -14,7 +14,19 @@ export default function Home() {
       </div>
 
       <div>
-        <Map />
+        <div className="p-8">
+          <h1 className="font-bold md:text-3xl text-2xl">Our depositories</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+            venenatis lacinia vehicula.
+          </p>
+        </div>
+
+        <Map
+          center={{ lat: -28.061107159470325, lng: 153.36120285364683 }}
+          zoom={16}
+          style={{ width: "100%", height: "50dvh" }}
+        />
       </div>
     </main>
   );
