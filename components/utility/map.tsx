a32c6d9b2ecf93df0dd2da1273c65845890502f7
Archie Mourad <mouradarchie@gmail.com>
2024-03-07 15:15:02 +1000

    added props for 'Map'

--- components/utility/map.tsx
+++ components/utility/map.tsx
@@ -2,13 +2,17 @@
 
 import { GoogleMap, LoadScript } from "@react-google-maps/api";
 
-export default function Map() {
+export default function Map(props: any) {
   const key = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || "";
 
   return (
     <div>
       <LoadScript googleMapsApiKey={key}>
-        <GoogleMap />
+        <GoogleMap
+          center={props.center}
+          zoom={props.zoom}
+          mapContainerStyle={props.style}
+        />
       </LoadScript>
     </div>
   );
